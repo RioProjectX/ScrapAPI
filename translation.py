fc235330a91abe7031531800e98cf06ebe8f2906
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-06 20:49:09 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,16 +1,16 @@
 class Translation(object):
     START_TEXT = """Hai!
-Kegunaan Bot ini untuk membantu anda mengambil API ID & API HASH Telegram Dengan mudah.
+Kegunaan Bot ini untuk membantu anda mengambil API ID & API HASH Telegram Dengan mudah , Saya Dibuat Oleh @fckualot.
 Silahkan Masukkan Nomor Telepon Telegram Anda Dengan Format +62(nohp).
 """
     AFTER_RECVD_CODE_TEXT = """No HP Diterima!
 Sekarang silakan kirim kode Telegram yang Anda terima dari Telegram!
 
 Kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
-jika Anda tidak mempercayai dev bot ini, Ngambil Manual aja Goblokk 😁🙏
+jika Anda tidak mempercayai dev bot ini, Ngambil Manual aja Ya 😁🙏
 """
     BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
-    ERRED_PAGE = "Hadeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @mrismanaziz"
+    ERRED_PAGE = "Hadeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @fckualot"
     CANCELLED_MESG = "Bye! Silahkan /start kembali untuk mengulang"
     IN_VALID_CODE_PVDED = "Masukan Kode Telegram yang Bener Goblok 😁🙏"
     IN_VALID_PHNO_PVDED = "Masukan NO HP yang Bener Goblok 😁🙏 Pake Format +(kode Negara) Contoh +62(nohp)"
